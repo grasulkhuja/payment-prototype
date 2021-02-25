a35e2688a75282ade5338fe5494dab73aa3d8fa7
GRasul99 <rasulganiyev1999@gmail.com>
2021-02-25 21:54:04 +0500

    start point

---
+++ src/components/PaymentReceipt.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Receipt</div>
+</template>
+
+<script>
+export default {
+  name: 'PaymentReceipt'
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/Payment.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col container mx-auto">
+    <PaymentReceipt />
+    <PaymentForm />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Payment',
+  components: {
+    PaymentForm: () => import('@/components/PaymentForm'),
+    PaymentReceipt: () => import('@/components/PaymentReceipt')
+  }
+}
+</script>
