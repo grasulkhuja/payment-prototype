5c93a09b8ca1aee7dd01efe4913326d6fe051f83
GRasul99 <rasulganiyev1999@gmail.com>
2021-03-05 16:51:43 +0500

    Added hover effect to submit button

--- src/components/PaymentForm.vue
+++ src/components/PaymentForm.vue
@@ -68,7 +68,7 @@
       </div>
       <button
         type="submit"
-        class="block py-2 my-6 rounded-md bg-blue-700 bg-opacity-90 text-white w-full md:w-2/5 mx-auto"
+        class="block py-2 my-6 rounded-md bg-blue-700 bg-opacity-90 text-white w-full md:w-2/5 mx-auto hover:bg-blue-600 hover:bg-opacity-95"
       >
         Submit
       </button>
