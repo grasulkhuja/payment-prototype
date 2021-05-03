dbea40282555bd4b9e44337b765f488874a271ef
GRasul99 <rasulganiyev1999@gmail.com>
2021-05-03 14:43:12 +0500

    Used Base components in PaymentACSForm

--- src/components/PaymentACSForm.vue
+++ src/components/PaymentACSForm.vue
@@ -1,26 +1,15 @@
 <template>
   <div class="flex flex-col mt-4 items-center w-full shadow-md">
     <form @submit.prevent="submitOTPCode" class="flex flex-col">
-      <label class="mt-4 flex flex-col" for="otp">
-        <span class="block mb-2 text-gray-800 text-opacity-80">
-          Введите код подтверждения
-        </span>
-      </label>
-      <input
+      <base-input
+        label="Введите код подтверждения"
         type="text"
         id="otp"
-        class="rounded-lg w-full placeholder-gray-800 placeholder-opacity-75 block mb-4"
         placeholder="--- ---"
-        required
         v-model="otpCode"
         v-mask="masks.otpMask"
       />
-      <button
-        type="submit"
-        class="block py-2 my-6 rounded-md bg-blue-700 bg-opacity-90 text-white w-full md:w-2/5 mx-auto hover:bg-blue-600 hover:bg-opacity-95"
-      >
-        Submit
-      </button>
+      <base-button type="submit">Submit</base-button>
     </form>
     <form id="hiddenForm" method="post" :action="this.url" ref="acsForm">
       <input
